6efbcf1c8e5ed787f264def1f871759453eaeb41
swsolis <wssolisa2019@gmail.com>
2025-10-28 19:11:30 -0500

    Se añadio un nuevo respaldo

---
+++ facturacion_271025.sql
@@ -0,0 +1,197 @@
+-- MySQL dump 10.13  Distrib 8.3.0, for Win64 (x86_64)
+--
+-- Host: localhost    Database: facturacion
+-- ------------------------------------------------------
+-- Server version	8.3.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `_types_holder`
+--
+
+DROP TABLE IF EXISTS `_types_holder`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `_types_holder` (
+  `doc_estado` enum('borrador','emitido','enviado','autorizado','rechazado','anulado') DEFAULT NULL,
+  `producto_tipo` enum('bien','servicio') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `_types_holder`
+--
+
+LOCK TABLES `_types_holder` WRITE;
+/*!40000 ALTER TABLE `_types_holder` DISABLE KEYS */;
+/*!40000 ALTER TABLE `_types_holder` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `empresas`
+--
+
+DROP TABLE IF EXISTS `empresas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `empresas` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `ruc` varchar(13) NOT NULL,
+  `razon_social` varchar(200) NOT NULL,
+  `nombre_comercial` varchar(200) DEFAULT NULL,
+  `direccion_fiscal` varchar(300) DEFAULT NULL,
+  `regimen` varchar(100) DEFAULT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  `telefono` varchar(50) DEFAULT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT '1',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_empresas_ruc` (`ruc`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `empresas`
+--
+
+LOCK TABLES `empresas` WRITE;
+/*!40000 ALTER TABLE `empresas` DISABLE KEYS */;
+INSERT INTO `empresas` VALUES (1,'1803932563','SOLUCIONES TECNO_ID','SOLUCIONES WALKER','Av. República 202, Quito','Regimen General','contacto@solucioneswalker.com','0995465404',1,'2025-09-30 19:35:31','2025-10-14 19:21:01'),(2,'1790012345004','Empresa ABC Cía.',NULL,NULL,NULL,NULL,'0991234567',0,'2025-09-30 19:36:03','2025-10-27 20:30:09'),(3,'1790012345001','INDUSTRIAS ALFA S.A.','Alfa Foods','Av. Principal 123, Quito, Pichincha','Régimen General','contacto@alfa.com.ec','0987654321',1,'2025-10-07 19:43:57','2025-10-07 19:43:57'),(4,'1803005071001','EDGAR SOLIS','WEF SOLUTION','AV. GENERAL ENRIQUEZ Y SAN LUIS','REGIMEN GENERAL','efsolis@gmail.com','0984074616',1,'2025-10-14 19:33:10','2025-10-14 19:33:10');
+/*!40000 ALTER TABLE `empresas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `prueba_rol`
+--
+
+DROP TABLE IF EXISTS `prueba_rol`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `prueba_rol` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(80) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `prueba_rol`
+--
+
+LOCK TABLES `prueba_rol` WRITE;
+/*!40000 ALTER TABLE `prueba_rol` DISABLE KEYS */;
+INSERT INTO `prueba_rol` VALUES (1,'ADMINISTRADOR'),(2,'DOCENTE'),(3,'DOCENTE'),(4,'DOCENTE'),(5,'ADMINISTRADOR'),(6,'OPERARIO');
+/*!40000 ALTER TABLE `prueba_rol` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(80) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_roles_nombre` (`nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'ADMINISTRADOR'),(3,'DOCENTE');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sucursales`
+--
+
+DROP TABLE IF EXISTS `sucursales`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sucursales` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `empresa_id` bigint NOT NULL,
+  `nombre` varchar(120) NOT NULL,
+  `codigo` varchar(20) NOT NULL,
+  `direccion` varchar(300) DEFAULT NULL,
+  `punto_emision` varchar(10) DEFAULT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT '1',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_sucursales_empresa_codigo` (`empresa_id`,`codigo`),
+  CONSTRAINT `fk_sucursales_empresa` FOREIGN KEY (`empresa_id`) REFERENCES `empresas` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sucursales`
+--
+
+LOCK TABLES `sucursales` WRITE;
+/*!40000 ALTER TABLE `sucursales` DISABLE KEYS */;
+INSERT INTO `sucursales` VALUES (1,1,'Sucursal Norte','SN001','Av. 10 de Agosto y Colón, Quito','001',1,'2025-10-07 19:35:08','2025-10-07 19:35:08'),(2,1,'Sucursal Sur','SS002','Av. Maldonado y Morán Valverde, Quito','002',1,'2025-10-07 19:35:08','2025-10-07 19:35:08'),(3,1,'Sucursal Matriz Guayaquil','SUC001','Av. 9 de Octubre y Malecón Simón Bolívar, Edificio Torres del Río, Piso 5, Guayaquil','001',1,'2025-10-07 19:35:33','2025-10-07 19:35:33'),(5,1,'Sucursal Matriz Quito','SUC002','Av. Eloy Alfaro c3n45 y Rio Coca','002',1,'2025-10-07 19:43:24','2025-10-07 19:43:24');
+/*!40000 ALTER TABLE `sucursales` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `empresa_id` bigint NOT NULL,
+  `username` varchar(60) NOT NULL,
+  `hash_password` text NOT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT '1',
+  `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_usuarios_empresa_username` (`empresa_id`,`username`),
+  CONSTRAINT `fk_usuarios_empresa` FOREIGN KEY (`empresa_id`) REFERENCES `empresas` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,1,'admin','hashedpassword123','admin@abc.com',1,'2025-10-13 19:21:57','2025-10-13 19:21:57'),(3,2,'admin','ssolis','ssolis@abc.com',1,'2025-10-13 19:24:33','2025-10-13 19:24:33');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-10-27 20:57:23
